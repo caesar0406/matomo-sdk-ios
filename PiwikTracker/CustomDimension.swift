db4c0b731f45b02a7bb1596369e0de96e84c3a31
Truck Liu <caesar0406@gmail.com>
2018-02-05 17:45:15 +0800

    Custom get/set value function for the Dimension object.

--- PiwikTracker/CustomDimension.swift
+++ PiwikTracker/CustomDimension.swift
@@ -16,18 +16,33 @@ public struct CustomDimension {
 
 @objc public class CustomDimensionObj:NSObject {
     /// The index of the dimension. A dimension with this index must be setup in the piwik backend.
-    let index: Int
+    var index: Int
     
     /// The value you want to set for this dimension.
-    let value: String
+    var value: String
     
-    init(index: Int, value: String) {
-        self.index = index
-        self.value = value
+    public override init() {
+        self.index = 0
+        self.value = ""
         super.init()
     }
-
-    convenience override init() {
-        self.init(index: 0, value: "")
+    
+    @objc public func setItemValue(index: Int, value: String) {
+        self.index = index
+        self.value = value
+    }
+    
+    @objc public func getItemIndex()->Int {
+        return self.index
+    }
+    
+    @objc public func getItemValue()->String {
+        return self.value
+    }
+    
+    public convenience init(index: Int, value: String) {
+        self.init()
+        self.index = index
+        self.value = value
     }
 }
